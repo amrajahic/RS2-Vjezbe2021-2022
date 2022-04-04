fc05a952248783db6c7b29151bf81b06783592b6
amrahindic <amrahindiic@gmail.com>
2022-04-04 22:48:23 +0200

    Vjezba 2
    Scaffold-DbContext
    Dependency injection
    Auto Mapper

---
+++ eProdaja310322/eProdaja.Services/IProizvodiService.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public interface IProizvodiService /* interface koji ce biti odgovoran da dobavi proizvode */
+    {
+        IEnumerable<Models.Proizvodi> Get();
+
+        public Models.Proizvodi GetById(int Id);
+    }
+}
---
+++ eProdaja310322/eProdaja310322/Controllers/KorisniciController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using eProdaja.Services;
+using eProdaja.Services.Database;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace eProdaja310322.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KorisniciController : ControllerBase
+    {
+        private readonly IKorisniciService _korisniciService;
+
+        public KorisniciController(IKorisniciService korisniciService)
+        {
+            _korisniciService = korisniciService;
+        }
+
+        [HttpGet]
+        public IEnumerable<eProdaja.Models.Korisnici> Get()
+        {
+            return _korisniciService.Get();
+        }
+
+        
+    }
+}
